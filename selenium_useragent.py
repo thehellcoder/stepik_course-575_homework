87a4fda573481d53d4458e4996871b38cf47e9db
thehellcoder <the.hell.coder@gmail.com>
2019-08-16 13:50:39 +0300

    now I know what's the UserAgent of Mozilla Firefox running under Selenium

--- selenium_useragent.py
+++ selenium_useragent.py
@@ -1,7 +1,16 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('https://www.whatsmyua.info/')
+def print_user_agent(browser_name):
+    browser = None
+    if browser_name == 'firefox':
+        browser = webdriver.Firefox()
+    else:
+        browser = webdriver.Chrome()
+    browser.get('https://www.whatsmyua.info/')
+    custom_ua_string = browser.find_element_by_id('custom-ua-string')
+    print('[*] {} = {}'.format(browser_name, custom_ua_string.text))
+    browser.quit()
 
-custom_ua_string = browser.find_element_by_id('custom-ua-string')
-print(custom_ua_string.text)
+if __name__ == '__main__':
+    print_user_agent('chrome')
+    print_user_agent('firefox')
